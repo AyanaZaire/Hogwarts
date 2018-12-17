9b73687e4e1a91b03acc9117ac6cfd668d5728f1
github username <github email address>
2018-12-17 17:49:13 -0500

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CharacterContainer from "./Containers/CharacterContainer";
+import HouseEditForm from "./Components/HouseEditForm";
+import Form from "./Components/NewCharacterForm";
+import SearchForm from "./Components/SearchForm";
+import HouseListContainer from "./Containers/HouseListContainer";
 
 class App extends Component {
+  state = {
+    characters: [],
+    filteredArr: [],
+    character: {},
+    houseSelected: "",
+    searchTerm: ""
+  };
+
+  componentDidMount() {
+    //fetch the data from db JSON
+    fetch("http://localhost:3001/potter-stuff")
+      .then(res => res.json())
+      .then(charList =>
+        this.setState({
+          characters: charList,
+          filteredArr: charList
+        })
+      );
+  }
+
+  handleHouseChange = e => {
+    console.log(e);
+    //find specific character based on click
+    let house = e.target.value;
+    this.setState({
+      houseSelected: house
+    });
+
+    //option 1:
+    //grab the specific character based on name
+    let newArr = [...this.state.characters];
+    //find the index of that character of the obj name you stored in state.
+    // let indexOfChar = newArr.findIndex(
+    //   y => y.name === this.state.character.name
+    // );
+    //find char obj via === to character in state
+    let character = newArr.find(char => char === this.state.character);
+    console.log("chracter", character);
+    //change the house of the specifc characer to the hosue Selected.
+    character.house = house;
+    console.log("character after changing house", character);
+  };
+
+  handleCharacterClick = charObj => {
+    this.setState({
+      character: charObj
+    });
+  };
+
+  handleSubmit = newChar => {
+    let newArr = [...this.state.characters, newChar];
+    this.setState({
+      characters: newArr
+    });
+  };
+
+  searchChangeHandler = e => {
+    let term = e.target.value;
+    let newArr = [...this.state.characters].filter(char =>
+      char.name.includes(term)
+    );
+
+    this.setState({
+      searchTerm: e.target.value,
+      filteredArr: newArr
+    });
+  };
+  //change the props for that character
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Form submitHandler={this.handleSubmit} />
+        <br />
+        <SearchForm
+          searchTerm={this.state.searchTerm}
+          changeHandler={this.searchChangeHandler}
+        />
+        <CharacterContainer
+          characters={this.state.filteredArr}
+          handleCharacterClick={this.handleCharacterClick}
+        />
+        <HouseEditForm
+          character={this.state.filteredArr}
+          handleHouseChange={this.handleHouseChange}
+        />
+        <HouseListContainer
+          characters={this.state.filteredArr}
+          handleCharacterClick={this.handleCharacterClick}
+        />
       </div>
     );
   }
